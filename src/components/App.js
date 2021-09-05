8d492962a7b1551ee50ec26002ad4b5ac26c8934
Dimitris Moulas <d.moulas@atc.gr>
2021-09-05 09:56:04 +0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/styles.scss';
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    return(
+        <div className="App">
+            Hello
+        </div>
+    )
+}
